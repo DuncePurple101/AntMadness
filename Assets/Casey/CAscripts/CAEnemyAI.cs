3dad409c8e544aed572c6aa66788048804deaac4
MUKILTEO\1610424 <1610424@mukilteo.wednet.edu>
2022-03-23 10:12:27 -0700

    Added AI follow script. My new record.

---
+++ Assets/Casey/CAscripts/CAEnemyAI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAEnemyAI : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float RetreatDistance;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && (Vector2.Distance(transform.position, player.position) > RetreatDistance))
+        {
+            transform.position = this.transform.position;
+
+        }else if (Vector2.Distance(transform.position, player.position) < RetreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Casey/CAscripts/CAplayermove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAplayermove : MonoBehaviour
+{
+	private float speed = 10.0f;
+    public GameObject charcter;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+	}
+}
