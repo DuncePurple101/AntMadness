7058c5f54ae279af66eb7af7fedd8220b45b08c6
MUKILTEO\1711757 <1711757@mukilteo.wednet.edu>
2022-03-25 10:20:12 -0700

    screwed up timer

---
+++ Assets/Keon/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeValue = 300;
+    public Text timeText;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeValue > 0)
+        {
+            timeValue -= Timer.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if(timeToDisplay <0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorTItn(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
