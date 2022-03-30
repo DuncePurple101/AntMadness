1708fb49a58cd3179d12cadf2b268b2840a2c63f
MUKILTEO\1610424 <1610424@mukilteo.wednet.edu>
2022-03-30 09:22:14 -0700

    Egg and player can die

---
+++ Assets/Casey/CAscripts/CAEgg.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAEgg : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public EggHealthBar EggHealthBar;
+
+
+    // Start is called bdefore the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        EggHealthBar.Setmaxhealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            TakeDamage(20);
+            //Debug.Log("Protect the egg");
+        }
+    }
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        EggHealthBar.SetHealth(currentHealth);
+    }
+}
