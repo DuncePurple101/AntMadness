c222533032b218a647b39840412069aea0ec951d
MUKILTEO\1610424 <1610424@mukilteo.wednet.edu>
2022-04-14 07:54:50 -0700

    Added camera follow script to player.

---
+++ Assets/Casey/CAscripts/CAcamerafollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAcamerafollow : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 1, -5);
+    }
+}
