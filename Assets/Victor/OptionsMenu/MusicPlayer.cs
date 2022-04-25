72bd58219d980e98ff6d5a68fb0c1bb34344e344
MUKILTEO\1710204 <1710204@mukilteo.wednet.edu>
2022-04-25 10:16:28 -0700

    Volume Slider, Credits, Music in other scenes

---
+++ Assets/Victor/OptionsMenu/MusicPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public Slider volumeSlider;
+
+    //Value frp, the slider, and it convert to volume level
+    private float MusicVolume = .1f;
+
+
+    private void Start()
+    {
+        AudioSource.Play();
+        MusicVolume = PlayerPrefs.GetFloat("volume");
+        AudioSource.volume = MusicVolume;
+        //volumeSlider.value = MusicVolume;
+    }
+
+
+
+    private void Update()
+    {
+        AudioSource.volume = MusicVolume;
+        PlayerPrefs.SetFloat("volume", MusicVolume);
+    }
+    public void updateVolume(float volume)
+    {
+        MusicVolume = volume;
+    }
+    public void MusicReset()
+    {
+        PlayerPrefs.DeleteKey("volume");
+        AudioSource.volume = .1f;
+        volumeSlider.value = .1f;
+
+    }
+}
--- Assets/Victor/OptionsMenu/SettingsMenu.cs
+++ Assets/Victor/OptionsMenu/SettingsMenu.cs
@@ -7,11 +7,11 @@ using UnityEngine.Audio;
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    public void SetVolume(float volume)
-    {
-        Debug.Log(volume);
-        //audioMixer.SetFloat("volume", volume);
-    }
+    //public void SetVolume(float volume)
+    //{
+    //    Debug.Log(volume);
+    //    audioMixer.SetFloat("volume", volume);
+    //}
     public void SetFullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
