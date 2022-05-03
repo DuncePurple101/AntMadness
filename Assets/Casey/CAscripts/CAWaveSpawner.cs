d13e6aeff2c06edb5927ddf49ce656a92b4a7942
MUKILTEO\1610424 <1610424@mukilteo.wednet.edu>
2022-05-03 08:03:42 -0700

    pushing changes from yesterday. added an enemy wave spawn

---
+++ Assets/Casey/CAscripts/CAWaveSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+
+public class Wave
+{
+    public string waveName;
+    public int noOfEnemies;
+    public GameObject[] typeOfEnemies;
+    public float spawnInterval;
+}
+
+public class CAWaveSpawner : MonoBehaviour
+{
+    public Wave[] waves;
+    public Transform[] spawnPoints;
+
+    private Wave currentWave;
+    private int currentWaveNumber;
+
+    private bool canSpawn = true;
+
+    private void Update()
+    {
+        currentWave = waves[currentWaveNumber];
+        SpawnWave();
+    }
+
+    void SpawnWave()
+    {
+        if (canSpawn)
+        {
+            GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
+            Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
+            currentWave.noOfEnemies--;
+            if (currentWave.noOfEnemies == 0)
+            {
+                canSpawn = false;
+            }
+        }
+    }
+
+}
