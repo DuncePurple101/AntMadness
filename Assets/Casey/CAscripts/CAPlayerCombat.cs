86a466ca5077c94cf01a6be1261a18f503241ce4
MUKILTEO\1710204 <1710204@mukilteo.wednet.edu>
2022-05-09 10:20:13 -0700

    Changed Attack to SpaceBar

--- Assets/Casey/CAscripts/CAPlayerCombat.cs
+++ Assets/Casey/CAscripts/CAPlayerCombat.cs
@@ -13,7 +13,7 @@ public class CAPlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Attack();
         }
